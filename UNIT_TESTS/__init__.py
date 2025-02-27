f11c09dc677ee56a91d23db22bf904d68fce82b5
beelerpayton <beelerpayton@hotmail.com>
2025-02-27 11:14:04 -0700

    remove old files

--- UNIT_TESTS/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 27 10:23:42 2024
-
-@author: beel083
-"""
-
--- multipart/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 12 09:15:32 2024
-
-@author: fier887
-"""
-
