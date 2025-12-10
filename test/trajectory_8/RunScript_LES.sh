cc95adc071cff3a326ba3b28f393939f97beb29e
beelerpayton <beelerpayton@hotmail.com>
2025-12-10 13:55:17 -0700

    Backup.

--- test/trajectory_8/RunScript_LES.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-python3 ../../multipart/SPLAT_initialization.py 100 ../../datasets/parcel_traces_0425_15utc /Users/beel083/Library/CloudStorage/OneDrive-PNNL/Desktop/codes/multipart_archived/test/trajectory_8 818
-python3 ../../multipart/MAIN_les.py ../../datasets/parcel_traces_0425_15utc /Users/beel083/Library/CloudStorage/OneDrive-PNNL/Desktop/codes/multipart_archived/test/trajectory_8
